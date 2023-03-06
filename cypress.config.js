341ae35f846bd2537c804f6ff8b3acd4f7a7679f
TatsianaLohinava <tlohinava@solvd.com>
2023-03-06 17:06:30 +0300

    projectId was updated

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'k8z923',
+  projectId: 'xn41gq',
   blockHosts: [
     '*.yandex.ru',
     '*.google-analytics.com'
